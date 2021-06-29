32a596658568adebd9105d8f2192aeb537d50fb0
YuanSamuel <samuel6161@gmail.com>
2021-06-28 20:14:12 -0500

    updates

--- components/layout.js
+++ components/layout.js
@@ -22,9 +22,12 @@ export default function Layout({ children, pageTitle }) {
         <main className='w-11/12 md:w-full max-w-2xl mx-auto my-8 flex-grow'>
           {children}
         </main>
-        <footer className='flex flex-col items-center w-full h-24 border-t border-blue-400'>
+        <footer className='flex flex-col items-center w-full h-16 border-t border-blue-400'>
           <div className='w-11/12 md:w-full max-w-3xl m-auto flex flex-row items-center justify-center'>
-            All content © Me
+            Developed with 🔌
+            <a className='pl-8 text-blue-500' href='mailto:syuan@utexas.edu'>
+              Contact me
+            </a>
           </div>
         </footer>
       </div>
@@ -36,15 +39,11 @@ function AppNav() {
   return (
     <nav className='text-2xl text-gray-600'>
       <ActiveLink href='/' activeClassName='text-blue-400'>
-        <a className='hover:text-blue-400 mr-8'>
-          Home
-        </a>
+        <a className='hover:text-blue-400 mr-8'>Home</a>
       </ActiveLink>
       <ActiveLink href='/blog' activeClassName='text-blue-400'>
-        <a className='hover:text-blue-400'>
-          Blog
-        </a>
+        <a className='hover:text-blue-400'>Blog</a>
       </ActiveLink>
     </nav>
-  )
+  );
 }
